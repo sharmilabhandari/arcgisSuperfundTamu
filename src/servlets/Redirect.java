f148d7fe8c097bcdf968957dd2693135f179f682
sharmilabhandari <sharmilatbhandari@gmail.com>
2019-04-10 11:46:58 -0500

    initial commit

---
+++ src/servlets/Redirect.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Redirect
+ */
+@WebServlet("/Redirect")
+public class Redirect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public Redirect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path = request.getParameter("path");
+		if (path.equals("about")) {
+			request.getServletContext().getRequestDispatcher("/WEB-INF/about.html").forward(request, response);
+		} else if (path.equals("downloadData")) {
+			request.getServletContext().getRequestDispatcher("/WEB-INF/downloadData.html").forward(request, response);
+
+		} else if (path.equals("home")) {
+			request.getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
